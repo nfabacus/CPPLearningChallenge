b3a936fab385eab2c20c3366b6703872c823fac9
Noby Fujioka <n.fujioka@abacusmaths.info>
2018-11-15 20:14:41 +0000

    15 nov 2018

--- README.md
+++ README.md
@@ -6,4 +6,5 @@ This repo is for C++.
 
 Initial challenge - go through a Udemy course on C++ for around 15 minutes daily.
 
-13th - 5th Nov, 2018 - Set up IDE Clion. Start over again the Udemy course on C++ I did not finish previously. Start with hello world and variables.
+13th - 15th Nov, 2018 - Set up IDE Clion. Start over again the Udemy course on C++ I did not finish previously. Start with hello world and variables.
+15th Nov, 2018 - variable types. Arithmetic operators are the same as other languages.
--- main.cpp
+++ main.cpp
@@ -13,7 +13,22 @@ int main () {
     cout << A << endl;  // value of variable A changed
     cout << &A << endl; // but the memory address of the variable is still the same.
 
+    int a = 40, b = 12, c = 20;
+    cout << "a : " << a << " address: " << &a << endl;
+    cout << "b : " << b << " address: " << &b << endl;
+
+    int d;
+    cin >> d;
+    cout << endl;
+    cout << "variable d is " << d<< endl;
 
 
+    string name, surname;
+
+    cout << "What is your first name? " << endl;
+    cin >> name;
+    cout << "What is your last name? " << endl;
+    cin >> surname;
+    cout << "Hello, " << name << " " << surname << "!" << endl;
     return 0;
 }
