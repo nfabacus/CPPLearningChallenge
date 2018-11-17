3a12691d7a3c262637a71b88373a92379a0eee55
Noby Fujioka <n.fujioka@abacusmaths.info>
2018-11-17 23:07:05 +0000

    relational and logical operators

--- main.cpp
+++ main.cpp
@@ -17,6 +17,7 @@ int main () {
     cout << "a : " << a << " address: " << &a << endl;
     cout << "b : " << b << " address: " << &b << endl;
 
+    cout << "what is your integer? ";
     int d;
     cin >> d;
     cout << endl;
@@ -30,5 +31,17 @@ int main () {
     cout << "What is your last name? " << endl;
     cin >> surname;
     cout << "Hello, " << name << " " << surname << "!" << endl;
+
+    // relational operators
+    int firstInt = 10;
+    int secondInt = 5;
+
+    cout << (firstInt == secondInt) << endl;  //returns 0 for false. make sure to wrap a==b with parentheses. Otherwise, it will error.
+    cout << (firstInt != secondInt) << endl;  // returns 1 for true.
+    cout << (firstInt > secondInt) << endl;
+
+    cout << (firstInt != secondInt && firstInt > secondInt) << endl;
+    cout << (firstInt == secondInt || firstInt > secondInt) << endl;
+    cout << (firstInt == secondInt || firstInt < secondInt) << endl;
     return 0;
 }
