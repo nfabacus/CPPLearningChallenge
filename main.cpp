79a0c3122f0647cb0053e1d0bc6b284bb600f757
Noby Fujioka <n.fujioka@abacusmaths.info>
2018-11-26 23:56:18 +0000

    adds for loop

--- main.cpp
+++ main.cpp
@@ -90,5 +90,9 @@ int main () {
     cout << myMArr[0][0] << endl;
     cout << myMArr[2][4] << endl;
 
+    for (int i = 0; i < 10; i++) {
+        cout << "i is " << i << endl;
+    }
+
     return 0;
 }
